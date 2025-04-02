44241ad83cec3620e137f6ae8d9c7c3eedf9d01a
juanc <juancarlosbatistazeferino@gmail.com>
2025-04-01 22:43:07 -0300

    feat: add mode toggle

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,6 +7,7 @@ import { Sheet, SheetClose, SheetContent, SheetDescription, SheetFooter, SheetHe
 import { Label } from './ui/label'
 import { Input } from './ui/input'
 import DatePicker from './DatePicker'
+import { ModeToggle } from './ModeToggle'
 
 
 type Props = {}
@@ -19,64 +20,69 @@ const Header = (props: Props) => {
     return (
         <header className="flex items-center justify-between px-4 py-3 relative">
             <div className="flex items-center">
-            <Sheet>
-                <SheetTrigger asChild>
-                    <Button variant="ghost" size="icon" className="text-white">
-                        <Menu style={{ height: '30px', width: '30px' }} />
-                        <span className="sr-only">Menu</span>
-                    </Button>
-                </SheetTrigger>
-                <SheetContent side='left' className='container mx-auto px-4'>
-                    <SheetHeader>
-                        <SheetTitle>Menu</SheetTitle>
-                        <SheetDescription>
-                            Encontre o que você procura por aqui
-                        </SheetDescription>
-                    </SheetHeader>
+                <Sheet>
+                    <SheetTrigger asChild>
+                        <Button variant="ghost" size="icon" className="text-white">
+                            <Menu style={{ height: '30px', width: '30px' }} />
+                            <span className="sr-only">Menu</span>
+                        </Button>
+                    </SheetTrigger>
+                    <SheetContent side='left' className='container mx-auto px-4'>
+                        <SheetHeader>
+                            <SheetTitle>Menu</SheetTitle>
+                            <SheetDescription>
+                                Encontre o que você procura por aqui
+                            </SheetDescription>
+                        </SheetHeader>
 
-                    <Button variant='outline'>
-                        Meus projetos
-                    </Button>
+                        <Button variant='outline'>
+                            Meus projetos
+                        </Button>
 
-                    <SheetFooter>
-                        <SheetClose asChild>
-                            <Button type="submit">Save changes</Button>
-                        </SheetClose>
-                    </SheetFooter>
-                </SheetContent>
-            </Sheet>
+                        <SheetFooter>
+                            <SheetClose asChild>
+                                <Button type="submit">Save changes</Button>
+                            </SheetClose>
+                        </SheetFooter>
+                    </SheetContent>
+                </Sheet>
 
-            <img src="menota.svg" alt="" className='w-[150px]' />
+                <img src="menota.svg" alt="" className='w-[150px]' />
             </div>
 
-            <Sheet>
-                <SheetTrigger asChild>
-                    <Button variant="ghost" size="icon" className="text-white hover:bg-black/20">
-                        <Plus style={{ height: '30px', width: '30px' }} />
-                        <span className="sr-only">Add new</span>
-                    </Button>
-                </SheetTrigger>
-                <SheetContent side='right' className='container mx-auto px-4'>
-                    <SheetHeader>
-                        <SheetTitle>Criar Projeto</SheetTitle>
-                        <SheetDescription>
-                            Cadastre seu novo projeto na plataforma.
-                        </SheetDescription>
-                    </SheetHeader>
+            <div className="flex items-center">
 
-                    <Label>Título do Projeto</Label>
-                    <Input placeholder='Título' />
+                <ModeToggle />
 
-                    <DatePicker label='Data Prevista de Inicio' setDate={setDateInicio} />
-                    <DatePicker label='Data Prevista de Entrega' setDate={setDateEntrega} />
+                <Sheet>
+                    <SheetTrigger asChild>
+                        <Button variant="ghost" size="icon" className="text-white hover:bg-black/20">
+                            <Plus style={{ height: '30px', width: '30px' }} />
+                            <span className="sr-only">Add new</span>
+                        </Button>
+                    </SheetTrigger>
+                    <SheetContent side='right' className='container mx-auto px-4'>
+                        <SheetHeader>
+                            <SheetTitle>Criar Projeto</SheetTitle>
+                            <SheetDescription>
+                                Cadastre seu novo projeto na plataforma.
+                            </SheetDescription>
+                        </SheetHeader>
 
-                    <SheetFooter>
-                        <SheetClose asChild>
-                            <Button type="submit">Save changes</Button>
-                        </SheetClose>
-                    </SheetFooter>
-                </SheetContent>
-            </Sheet>
+                        <Label>Título do Projeto</Label>
+                        <Input placeholder='Título' />
+
+                        <DatePicker label='Data Prevista de Inicio' setDate={setDateInicio} />
+                        <DatePicker label='Data Prevista de Entrega' setDate={setDateEntrega} />
+
+                        <SheetFooter>
+                            <SheetClose asChild>
+                                <Button type="submit">Save changes</Button>
+                            </SheetClose>
+                        </SheetFooter>
+                    </SheetContent>
+                </Sheet>
+            </div>
         </header>
     )
 }
