e73a8b30dc569cd0d05631f95aa19c99dadece06
JUAN CARLOS BATISTA ZEFERINO <juan.zeferino@alunos.sc.senac.br>
2025-04-02 19:28:51 -0300

    feat: add componente toast

---
+++ src/components/SonnerToast.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import { forwardRef } from "react"
+import { Button } from "./ui/button"
+import { toast } from "sonner"
+
+type Props = {
+  variant?: "link" | "default" | "destructive" | "outline" | "secondary" | "ghost" | null | undefined
+  title: string
+  description: string
+  actionTitle: string
+  onClick: () => void
+  className?: string
+}
+
+const SonnerToast = forwardRef<HTMLButtonElement, Props>((props, ref) => {
+  return (
+    <Button
+      ref={ref}
+      variant={props.variant}
+      className={props.className}
+      onClick={() =>
+        toast(props.title, {
+          description: props.description,
+          action: {
+            label: props.actionTitle,
+            onClick: props.onClick,
+          },
+          position: 'top-left'
+        })
+      }
+    >
+      Show Toast
+    </Button>
+  )
+})
+
+SonnerToast.displayName = "SonnerToast"
+
+export default SonnerToast
