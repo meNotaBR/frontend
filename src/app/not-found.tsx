f44e13372e09e94eb66f3ba23134ee29a862c8f8
JUAN CARLOS BATISTA ZEFERINO <juan.zeferino@alunos.sc.senac.br>
2025-04-02 21:28:41 -0300

    feat: add component buttonm shadcn ui

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,18 @@
+import Header from '@/components/Header'
+import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 
 export default function NotFound() {
-  return (
+  return (<>
+    <Header />
     <div className='flex flex-col items-center justify-center min-h-screen gap-4'>
       <h2 className='text-2xl font-bold'>Página não encontrada</h2>
       <p>Não foi possível encontrar o recurso solicitado</p>
-      <Link href="/feed" className='underline hover:text-blue-600 transition-colors'>
-        Retornar ao feed
-      </Link>
+      <Button>
+        <Link href="/feed" className='transition-colors'>
+          Retornar ao feed
+        </Link>
+      </Button>
     </div>
-  )
+  </>)
 }
