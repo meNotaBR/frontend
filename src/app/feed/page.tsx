c0ae7a79c2488fcced9e727883e9864a3232e0c1
JUAN CARLOS BATISTA ZEFERINO <juan.zeferino@alunos.sc.senac.br>
2025-04-04 20:14:27 -0300

    feat: add chamada da api backend

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import Header from '@/components/Header'
 import ProjectCard from '@/components/ProjectCard'
 import ProjectsCarousel from '@/components/ProjectsCarousel'
@@ -10,36 +8,45 @@ import React from 'react'
 
 type Props = {}
 
-const page = (props: Props) => {
+const page = async (props: Props) => {
 
     const projetos = [
-        { nome: "Projeto 1", img: 'https://brandingforthepeople.com/wp-content/uploads/2019/04/Stock-Photography-vs-Real-Imagery.jpg', descricao: "Detalhes do projeto Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", startup: { nome: 'empresa tal', descricao: 'descricao startup', empresario: 'nome empresario' } },
-        { nome: "Projeto 2", img: 'https://brandingforthepeople.com/wp-content/uploads/2019/04/Stock-Photography-vs-Real-Imagery.jpg', descricao: "Detalhes do projeto Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", startup: { nome: 'empresa tal', descricao: 'descricao startup', empresario: 'nome empresario' } },
-        { nome: "Projeto 3", img: 'https://brandingforthepeople.com/wp-content/uploads/2019/04/Stock-Photography-vs-Real-Imagery.jpg', descricao: "Detalhes do projeto Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", startup: { nome: 'empresa tal', descricao: 'descricao startup', empresario: 'nome empresario' } },
-        { nome: "Projeto 4", img: 'https://brandingforthepeople.com/wp-content/uploads/2019/04/Stock-Photography-vs-Real-Imagery.jpg', descricao: "Detalhes do projeto Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", startup: { nome: 'empresa tal', descricao: 'descricao startup', empresario: 'nome empresario' } },
-        { nome: "Projeto 5", img: 'https://brandingforthepeople.com/wp-content/uploads/2019/04/Stock-Photography-vs-Real-Imagery.jpg', descricao: "Detalhes do projeto Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", startup: { nome: 'empresa tal', descricao: 'descricao startup', empresario: 'nome empresario' } },
+        { nome: "Projeto 1", img: 'https://t4.ftcdn.net/jpg/03/00/14/39/360_F_300143961_8kJTPiTbWallCIBxO0GQzoxgwE9cIRGG.jpg', descricao: "Detalhes do projeto Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", startup: { nome: 'empresa tal', descricao: 'descricao startup', empresario: 'nome empresario' } },
+        { nome: "Projeto 2", img: 'https://media.istockphoto.com/id/1395187689/photo/signing-contract-investor-and-contractor.jpg?s=612x612&w=0&k=20&c=SYD7JvzTDqcgz3i9eooa5PQY4650BIttb9batDCmE9Y=', descricao: "Detalhes do projeto Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", startup: { nome: 'empresa tal', descricao: 'descricao startup', empresario: 'nome empresario' } },
+        { nome: "Projeto 3", img: 'https://t4.ftcdn.net/jpg/03/00/14/39/360_F_300143961_8kJTPiTbWallCIBxO0GQzoxgwE9cIRGG.jpg', descricao: "Detalhes do projeto Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", startup: { nome: 'empresa tal', descricao: 'descricao startup', empresario: 'nome empresario' } },
+        { nome: "Projeto 4", img: 'https://t4.ftcdn.net/jpg/03/00/14/39/360_F_300143961_8kJTPiTbWallCIBxO0GQzoxgwE9cIRGG.jpg', descricao: "Detalhes do projeto Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", startup: { nome: 'empresa tal', descricao: 'descricao startup', empresario: 'nome empresario' } },
+        { nome: "Projeto 5", img: 'https://t4.ftcdn.net/jpg/03/00/14/39/360_F_300143961_8kJTPiTbWallCIBxO0GQzoxgwE9cIRGG.jpg', descricao: "Detalhes do projeto Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", startup: { nome: 'empresa tal', descricao: 'descricao startup', empresario: 'nome empresario' } },
     ]
+
+    const json = await fetch('http://localhost:8080/api/projeto/list')
+    const projects: Projeto[] = await json.json();
+
+    let projetosGeral: Projeto[] = projects.sort((projeto1, projeto2) => { 
+                 if(projeto1.upvote.length < projeto2.upvote.length) return 1; 
+                 if(projeto1.upvote.length > projeto2.upvote.length) return -1; 
+                 return 0; 
+             });
 
     return (<>
         <Header />
 
-        <ProjectsCarousel label='Projetos mais votados da semana' projetos={projetos} />
+        <ProjectsCarousel label='Projetos que mais subiram essa semana' projetos={projetos} />
 
         <Label className='mb-4 text-3xl'>Feed</Label>
         <div className='w-full'>
 
             <Tabs defaultValue='Para Você'>
                 <TabsList className='grid w-full grid-cols-2'>
-                    <TabsTrigger value='Para Você' >Para Você</TabsTrigger>
-                    <TabsTrigger value='Últimos Projetos' >Últimos Projetos</TabsTrigger>
+                    <TabsTrigger value='Para Você' className='hover:cursor-pointer' >Para Você</TabsTrigger>
+                    <TabsTrigger value='Últimos Projetos' className='hover:cursor-pointer'>Últimos Projetos</TabsTrigger>
                 </TabsList>
                 <TabsContent value='Para Você' className='grid lg:grid-cols-3 md:grid-cols-2 gap-4' >
-                    {projetos.map((projeto, index) => (
+                    {projetosGeral.map((projeto, index) => (
                         <ProjectCard projeto={projeto} key={index} />
                     ))}
                 </TabsContent>
                 <TabsContent value='Últimos Projetos' className='grid lg:grid-cols-3 md:grid-cols-2 gap-4'>
-                    {projetos.map((projeto, index) => (
+                    {projetosGeral.map((projeto, index) => (
                         <ProjectCard projeto={projeto} key={index} />
                     ))}
                 </TabsContent>
--- src/app/liked/page.tsx
+++ src/app/liked/page.tsx
@@ -5,15 +5,10 @@ import React from 'react'
 
 type Props = {}
 
-const page = (props: Props) => {
+const page = async (props: Props) => {
 
-    const projetos = [
-        { nome: "Projeto 1", img: 'https://brandingforthepeople.com/wp-content/uploads/2019/04/Stock-Photography-vs-Real-Imagery.jpg', descricao: "Detalhes do projeto Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", startup: { nome: 'empresa tal', descricao: 'descricao startup', empresario: 'nome empresario' } },
-        { nome: "Projeto 2", img: 'https://brandingforthepeople.com/wp-content/uploads/2019/04/Stock-Photography-vs-Real-Imagery.jpg', descricao: "Detalhes do projeto Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", startup: { nome: 'empresa tal', descricao: 'descricao startup', empresario: 'nome empresario' } },
-        { nome: "Projeto 3", img: 'https://brandingforthepeople.com/wp-content/uploads/2019/04/Stock-Photography-vs-Real-Imagery.jpg', descricao: "Detalhes do projeto Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", startup: { nome: 'empresa tal', descricao: 'descricao startup', empresario: 'nome empresario' } },
-        { nome: "Projeto 4", img: 'https://brandingforthepeople.com/wp-content/uploads/2019/04/Stock-Photography-vs-Real-Imagery.jpg', descricao: "Detalhes do projeto Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", startup: { nome: 'empresa tal', descricao: 'descricao startup', empresario: 'nome empresario' } },
-        { nome: "Projeto 5", img: 'https://brandingforthepeople.com/wp-content/uploads/2019/04/Stock-Photography-vs-Real-Imagery.jpg', descricao: "Detalhes do projeto Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", startup: { nome: 'empresa tal', descricao: 'descricao startup', empresario: 'nome empresario' } },
-    ]
+    const json = await fetch('http://localhost:8080/api/projeto/list')
+    const projects: Projeto[] = await json.json();
 
   return (<>
     
@@ -22,7 +17,7 @@ const page = (props: Props) => {
     <Label className='sm:text-4xl lg:text-3xl mb-4'>Projetos que você curtiu</Label>
     
     <div className='grid lg:grid-cols-3 md:grid-cols-2 gap-4'>
-        {projetos.map((projeto, index) => (
+        {projects.map((projeto, index) => (
             <ProjectCard projeto={projeto} key={index} />
         ))}
     </div>
