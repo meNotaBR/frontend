7338b4d9e84d58e8dc10d11f30c5a927dc70718d
JUAN CARLOS BATISTA ZEFERINO <juan.zeferino@alunos.sc.senac.br>
2025-04-04 20:12:55 -0300

    feat: add contagem de likes e busca na api

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,38 +1,144 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect, useState } from 'react'
 import { Card, CardContent, CardFooter, CardHeader } from './ui/card'
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 import { Label } from './ui/label'
 import { Badge } from './ui/badge'
 import { Button } from './ui/button'
-import { ArrowUpFromLine, Share2 } from 'lucide-react'
+import { ArrowUpFromLine, CalendarDays, Check, Share2 } from 'lucide-react'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './ui/tooltip'
+import { Separator } from './ui/separator'
+import { HoverCard, HoverCardContent, HoverCardTrigger } from './ui/hover-card'
 
 type Props = {
-    projeto: any
+    projeto: Projeto
 }
 
 const ProjectCard = (props: Props) => {
+
+    const [shared, setShared] = useState<boolean>(false)
+    const [liked, setLiked] = useState<boolean>()
+    const [totalLikes, setTotalLikes] = useState<number>(props.projeto.upvote.length)
+    const [openTooltip, setOpenTooltip] = useState(false)
+    const [openHoverCard, setOpenHoverCard] = useState(false)
+
+    const date = new Date(props.projeto.startup.dataCadastro)
+
+
+    const checkDescriptionSize = (desc: string) => {
+        let description: string;
+
+        if (desc.length > 225) {
+            return description = desc.substring(0, 224).concat('...');
+        }
+
+        return desc;
+    }
+
+    const sharedTrue = () => {
+        setShared(true);
+
+        setTimeout(() => { setShared(false) }, 2000)
+    }
+
+    const likeProject = () => {
+        props.projeto.liked = true;
+        setTotalLikes(totalLikes + 1);
+    }
+
+    const unlikeProject = () => {
+        props.projeto.liked = false;
+        setTotalLikes(totalLikes - 1);
+    }
+
+    useEffect(() => {
+        if (props.projeto && props.projeto.liked !== undefined) {
+            setLiked(props.projeto.liked);
+        }
+    }, [props.projeto?.liked]);
+
+
+
     return (
         <div>
             <Card>
                 <CardHeader className='flex'>
-                    <Avatar>
-                        <AvatarImage src='https://avatars.githubusercontent.com/u/113259173?v=4' alt='nome' />
-                        <AvatarFallback>JC</AvatarFallback>
-                    </Avatar>
-                    <Label className='mt-3'>{props.projeto.startup.nome}</Label>
-                    <Badge className='mt-2 w-20 h-5'>2 dias atrás</Badge>
+                    <div className="flex items-center gap-2">
+                        <Avatar>
+                            <AvatarImage src="" alt="nome" />
+                            <AvatarFallback>{props.projeto.startup.nomeFantasia.charAt(0)}</AvatarFallback>
+                        </Avatar>
+
+                        <HoverCard open={openHoverCard} onOpenChange={setOpenHoverCard}>
+                            <HoverCardTrigger asChild onClick={() => setOpenHoverCard(!openHoverCard)}>
+                                <Badge className="h-5 w-20">{props.projeto.startup.nomeFantasia}</Badge>
+                            </HoverCardTrigger>
+                            <HoverCardContent className="dark:bg-black ml-2">
+                                <div className="flex justify-between space-x-4">
+                                    <Avatar className=''>
+                                        <AvatarImage src="" />
+                                        <AvatarFallback>{props.projeto.startup.nomeFantasia.charAt(0)}</AvatarFallback>
+                                    </Avatar>
+                                    <div className="space-y-1">
+                                        <h4 className="text-sm font-semibold">{props.projeto.startup.nomeFantasia}</h4>
+                                        <p className="text-sm">
+                                            Descrição rapida da startup
+                                        </p>
+                                        <div className="flex items-center pt-2">
+                                            <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
+                                            <span className="text-xs text-muted-foreground">
+                                                {date.toLocaleDateString("pt-BR", { year: "numeric", month: "long" })}
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </HoverCardContent>
+                        </HoverCard>
+                        <Label className="ml-2">2 dias atrás</Label>
+                    </div>
+
+                    <TooltipProvider>
+                        <Tooltip open={openTooltip} onOpenChange={setOpenTooltip}>
+                            <TooltipTrigger asChild onClick={() => setOpenTooltip(!openTooltip)}>
+                                <Badge className="ml-auto mt-1 bg-black text-amber-50 pl-1">🔥Pelando</Badge>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                                <Label className='mb-2'>Os mais aquecidos da plataforma!🔥</Label>
+                                <p>Projetos que estão <b>pelando</b> recebem <b>upvotes dobrados!</b></p>
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+
                 </CardHeader>
-                <CardContent>
-                    <Label style={{ fontSize: '1.5em', marginBottom: '4px' }}>{props.projeto.nome}</Label>
-                    <Label className='mt-3'>{props.projeto.descricao}</Label>
+                <CardContent className='sm:max-h-[100px]'>
+                    <Label style={{ marginBottom: '4px' }} className='text-2xl xs:text-[20px]'>{props.projeto.nome}</Label>
+                    <Label className='mt-3'>{checkDescriptionSize(props.projeto.descricao)}</Label>
                 </CardContent>
-                <CardFooter className='flex justify-between'>
+                <CardFooter className='flex justify-between sm:mt-4'>
+
+                    {liked ? (
+                        <Button variant='secondary' className='rounded-2xl dark:bg-[#892be2] not-dark:bg-[#FF00FF]' onClick={() => unlikeProject()}>
+                            <ArrowUpFromLine /> {totalLikes}
+                        </Button>
+                    ) : (
+                        <Button variant='secondary' className='rounded-[15px]' onClick={() => likeProject()}>
+                            <ArrowUpFromLine /> {totalLikes}
+                        </Button>
+                    )}
+
                     <Button variant='secondary' className='rounded-2xl'>
-                        <ArrowUpFromLine /> 70
+                        Saiba mais
                     </Button>
-                    <Button variant='secondary' className='rounded-2x1'>
-                        <Share2 /> Share
-                    </Button>
+
+                    {shared ? (
+                        <Button variant='default' className='bg-green-400 hover:bg-green-400' style={{ borderRadius: '15px', fontSize: '0.8em' }}>
+                            <Check /> Link Copiado
+                        </Button>) :
+                        (
+                            <Button variant='secondary' style={{ borderRadius: '15px', fontSize: '0.8em' }} onClick={() => sharedTrue()}>
+                                <Share2 /> Compartilhar
+                            </Button>)}
                 </CardFooter>
             </Card>
         </div>
