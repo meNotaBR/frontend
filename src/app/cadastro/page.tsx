f48a4fc5225dde2b38b8e87393bd98068e41c7e7
JUAN CARLOS BATISTA ZEFERINO <juan.zeferino@alunos.sc.senac.br>
2025-04-07 21:22:37 -0300

    feat: add fetch post para cadastro no backend

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -1,25 +1,29 @@
+'use client'
+
 import { Button } from '@/components/ui/button'
 import { Checkbox } from '@/components/ui/checkbox'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectValue, SelectTrigger } from '@/components/ui/select'
-import React from 'react'
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
+import { toast } from 'sonner'
 type Props = {}
 
 const page = (props: Props) => {
 
-    const days: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+    const days: string[] = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31']
 
     const months = [
-        { value: "1", label: "Janeiro" },
-        { value: "2", label: "Fevereiro" },
-        { value: "3", label: "Março" },
-        { value: "4", label: "Abril" },
-        { value: "5", label: "Maio" },
-        { value: "6", label: "Junho" },
-        { value: "7", label: "Julho" },
-        { value: "8", label: "Agosto" },
-        { value: "9", label: "Setembro" },
+        { value: "01", label: "Janeiro" },
+        { value: "02", label: "Fevereiro" },
+        { value: "03", label: "Março" },
+        { value: "04", label: "Abril" },
+        { value: "05", label: "Maio" },
+        { value: "06", label: "Junho" },
+        { value: "07", label: "Julho" },
+        { value: "08", label: "Agosto" },
+        { value: "09", label: "Setembro" },
         { value: "10", label: "Outubro" },
         { value: "11", label: "Novembro" },
         { value: "12", label: "Dezembro" },
@@ -30,8 +34,117 @@ const page = (props: Props) => {
         { value: "2", label: "Empresário" }
     ]
 
-    const currentYear = new Date().getFullYear()
-    const years = Array.from({ length: currentYear - 1949 }, (_, i) => currentYear - i)
+    const currentYear = new Date().getFullYear();
+    const years = Array.from({ length: currentYear - 1949 }, (_, i) => currentYear - i);
+
+    const [nome, setNome] = useState<string>('');
+    const [sobrenome, setSobrenome] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [senha, setSenha] = useState<string>('');
+    const [cpf, setCpf] = useState<string>('');
+    const [contato, setContato] = useState<string>('');
+    const [dia, setDia] = useState<string>('');
+    const [mes, setMes] = useState<string>('');
+    const [ano, setAno] = useState<string>('');
+
+    const router = useRouter();
+
+    const [userType, setUserType] = useState<string>('');
+
+    const onChangeName = (event: React.ChangeEvent<HTMLInputElement>) => {
+        event.preventDefault();
+        setNome(event.target.value);
+    }
+
+    const onChangeSobrenome = (event: React.ChangeEvent<HTMLInputElement>) => {
+        event.preventDefault();
+        setSobrenome(event.target.value);
+    }
+
+    const onChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+        event.preventDefault();
+        setEmail(event.target.value);
+    }
+
+    const onChangeSenha = (event: React.ChangeEvent<HTMLInputElement>) => {
+        event.preventDefault();
+        setSenha(event.target.value);
+    }
+
+    const onChangeCpf = (event: React.ChangeEvent<HTMLInputElement>) => {
+        event.preventDefault();
+        setCpf(event.target.value);
+    }
+
+    const onChangeContato = (event: React.ChangeEvent<HTMLInputElement>) => {
+        event.preventDefault();
+        setContato(event.target.value);
+    }
+
+    const onChangeDia = (dia: string) => {
+        setDia(dia);
+    }
+
+    const onChangeMes = (mes: string) => {
+        setMes(mes);
+    }
+
+    const onChangeAno = (ano: string) => {
+        setAno(ano);
+    }
+
+    const concatDates = (dia: string, mes: string, ano: string) => {
+        return `${ano}-${mes}-${dia}`;
+    }
+
+    const postCadastro = async () => {
+        var url = '';
+
+        const cadastro: EmpresarioRequest = {
+            nome: nome,
+            sobrenome: sobrenome,
+            email: email,
+            senha: senha,
+            cpf: cpf,
+            dataNasc: concatDates(dia, mes, ano)
+        }
+        if (userType == '1') {
+            url = 'http://localhost:8080/api/investor/create'
+
+        }else {
+            cadastro.numeroCelular = contato;
+            url = 'http://localhost:8080/api/empresario/create'
+        }
+
+        console.log('url: ' + url);
+        console.log('cadastro: ' + JSON.stringify(cadastro));
+        
+        
+
+        const json = await fetch(url, { method: 'POST', headers: {'Content-type': 'application/json'}, body: JSON.stringify(cadastro)});
+
+        if (!json.ok) {
+            const errorData = await json.json().catch(() => null)
+            throw new Error(errorData?.erro || 'Ocorreu algum erro em seu cadastro');
+        }
+
+        if (json.status == 201) {
+            router.push('/login');
+        }
+    }
+
+    const handleCadastro = async (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+
+        toast.promise(postCadastro(), {
+            loading: "Efetuando seu cadastro... Por favor, aguarde!",
+            success: <b>Cadastro efetuado com sucesso!</b>,
+            error: (error: any) => {
+                return error.message
+            },
+            position: 'top-left'
+        })
+    }
 
     return (
         <div className='flex justify-center'>
@@ -40,18 +153,22 @@ const page = (props: Props) => {
                 <img src="menota.svg" alt="" className='mb-8' />
                 <Label style={{ fontSize: '35px' }} className='flex justify-center mb-5' >Cadastre-se</Label>
                 <div className='grid grid-cols-2 gap-1 w-full'>
-                    <Input type='text' placeholder='Nome' className='rounded-2xl h-11' />
-                    <Input type='text' placeholder='Sobrenome' className='rounded-2xl h-11' />
+                    <Input type='text' placeholder='Nome' className='rounded-2xl h-11' onChange={onChangeName}/>
+                    <Input type='text' placeholder='Sobrenome' className='rounded-2xl h-11' onChange={onChangeSobrenome}/>
                 </div>
-                <Input type='email' placeholder='Email' className='rounded-2xl h-11' />
-                <Input type='text' placeholder='CPF' className='rounded-2xl h-11' />
+                <Input type='email' placeholder='Email' className='rounded-2xl h-11' onChange={onChangeEmail}/>
+                <Input type='text' placeholder='CPF' className='rounded-2xl h-11' onChange={onChangeCpf}/>
+                
+                {userType == '2' ? (
+                    <Input type='text' placeholder='Contato' className='rounded-2xl h-11' onChange={onChangeContato}/>
+                ): ''}
 
 
                 <div>
                     <Label className='flex justify-center mb-5 mt-2'>Data de Nascimento</Label>
                     <div className='grid grid-cols-3 gap-1'>
 
-                        <Select>
+                        <Select onValueChange={onChangeDia}>
                             <SelectTrigger className='w-full rounded-2xl h-11'>
                                 <SelectValue placeholder='Dia' />
                             </SelectTrigger>
@@ -65,7 +182,7 @@ const page = (props: Props) => {
                             </SelectContent>
                         </Select>
 
-                        <Select>
+                        <Select onValueChange={onChangeMes}>
                             <SelectTrigger className='w-full rounded-2xl h-11'>
                                 <SelectValue placeholder='Mês' />
                             </SelectTrigger>
@@ -81,7 +198,7 @@ const page = (props: Props) => {
                             </SelectContent>
                         </Select>
 
-                        <Select>
+                        <Select onValueChange={onChangeAno}>
                             <SelectTrigger className='w-full rounded-2xl h-11'>
                                 <SelectValue placeholder='Ano' />
                             </SelectTrigger>
@@ -99,7 +216,7 @@ const page = (props: Props) => {
 
                     </div>
                 </div>
-                <Select>
+                <Select onValueChange={setUserType}>
                     <SelectTrigger className='w-full rounded-2xl h-11'>
                         <SelectValue placeholder='Posição' />
                     </SelectTrigger>
@@ -107,13 +224,13 @@ const page = (props: Props) => {
                         <SelectGroup>
                             <SelectLabel>Posição</SelectLabel>
                             {tipoCadastro.map((element) => (
-                                <SelectItem value={element.label} key={element.value}>{element.label}</SelectItem>
+                                <SelectItem value={element.value} key={element.value} defaultValue='1'>{element.label}</SelectItem>
                             ))}
                         </SelectGroup>
                     </SelectContent>
                 </Select>
 
-                <Input type='password' placeholder='Senha' className='rounded-2xl h-11' />
+                <Input type='password' placeholder='Senha' className='rounded-2xl h-11' onChange={onChangeSenha} />
                 <Input type='password' placeholder='Repita a senha' className='rounded-2xl h-11' />
 
                 <div className="flex items-center space-x-2">
@@ -127,7 +244,7 @@ const page = (props: Props) => {
                 </div>
 
                 <div className='mt-10 flex justify-center'>
-                    <Button className='w-50 font-bold h-15 rounded-2xl text-lg'>Cadastrar-se</Button>
+                    <Button className='w-50 font-bold h-15 rounded-2xl text-lg' onClick={handleCadastro}>Cadastrar-se</Button>
                 </div>
 
                 <Label className='flex justify-center mt-4 text-blue-600'> Já tem uma conta ? <Label className='underline'>Faça login</Label></Label>
