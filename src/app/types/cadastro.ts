dd50cb8edd774e93e50642048628a8fc8eccdc36
juanc <juancarlosbatistazeferino@gmail.com>
2025-04-12 20:15:04 -0300

    remove: retira type EmpresarioRequest

--- src/app/types/cadastro.ts
+++
@@ -1,9 +0,0 @@
-interface EmpresarioRequest {
-    nome: string,
-    sobrenome: string,
-    email: string,
-    senha: string,
-    cpf: string,
-    numeroCelular?: string,
-    dataNasc: string
-}
