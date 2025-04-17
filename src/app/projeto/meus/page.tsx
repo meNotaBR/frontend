f99c8ec1b5a26fb83339dc7f9b0c194d19757933
Carrerogabriel <132373775+Carrerogabriel@users.noreply.github.com>
2025-04-16 21:01:24 -0300

    Feat: Add page meus projeots

--- src/app/projeto/meus/page.tsx
+++ src/app/projeto/meus/page.tsx
@@ -1,13 +1,37 @@
+import Header from '@/components/Header';
+import ProjectCard from '@/components/ProjectCard'
+import { Label } from '@/components/ui/label';
+import { cookies } from 'next/headers';
 import React from 'react'
 
 type Props = {}
 
-const page = (props: Props) => {
-  return (
-    <div>
+const page = async (props: Props) => {
+
+    const token = ((await cookies()).get('token'))?.value;
+
+    const json = await fetch('http://localhost:8080/api/projeto/user', {
+        method: 'GET',
+        headers: {
+            "Authorization": `Bearer ${token}`,
+            "Content-type": "application/json"
+        }
+    })
+    const projects: Projeto[] = await json.json();
+
+    return (<>
+    
+      <Header />
+  
+      <Label className='sm:text-4xl lg:text-3xl mb-4'>Seus projetos</Label>
       
-    </div>
-  )
+      <div className='grid lg:grid-cols-3 md:grid-cols-2 gap-4'>
+          {projects.map((projeto, index) => (
+              <ProjectCard projeto={projeto} key={index} />
+          ))}
+      </div>
+      
+      </>)
 }
 
 export default page
