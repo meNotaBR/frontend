6888163e618aae4ddd3e488ba2ed3daa6ae3744d
juan.zeferino <juancarlosbatistazeferino@gmail.com>
2025-04-24 20:02:36 -0300

    feat: add component card para criar projeto

--- src/app/projeto/meus/page.tsx
+++ src/app/projeto/meus/page.tsx
@@ -1,6 +1,9 @@
 import Header from '@/components/Header';
 import ProjectCard from '@/components/ProjectCard'
+import { Card, CardContent } from '@/components/ui/card';
 import { Label } from '@/components/ui/label';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
+import { Plus } from 'lucide-react';
 import { cookies } from 'next/headers';
 import React from 'react'
 
@@ -20,18 +23,34 @@ const page = async (props: Props) => {
     const projects: Projeto[] = await json.json();
 
     return (<>
-    
-      <Header />
-  
-      <Label className='sm:text-4xl lg:text-3xl mb-4'>Seus projetos</Label>
-      
-      <div className='grid lg:grid-cols-3 md:grid-cols-2 gap-4'>
-          {projects.map((projeto, index) => (
-              <ProjectCard projeto={projeto} key={index} token={token}/>
-          ))}
-      </div>
-      
-      </>)
+
+        <Header />
+
+        <Label className='sm:text-4xl lg:text-3xl mb-4'>Seus projetos</Label>
+
+        <div className='grid lg:grid-cols-3 md:grid-cols-2 gap-4'>
+
+            <Card className='items-center justify-center hover:border-primary/30 transition-all duration-300 max-h-[282px] cursor-pointer'>
+                <TooltipProvider>
+                    <Tooltip>
+                        <TooltipTrigger asChild>
+                            <CardContent >
+                                <Plus className='w-30 h-30' />
+                            </CardContent>        
+                        </TooltipTrigger>
+                        <TooltipContent>
+                            <p>Criar novo projeto</p>
+                        </TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
+            </Card>
+
+            {projects.map((projeto, index) => (
+                <ProjectCard projeto={projeto} key={index} token={token} />
+            ))}
+        </div>
+
+    </>)
 }
 
 export default page
